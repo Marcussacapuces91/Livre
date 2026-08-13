035bc1ba8c2eb5cbeb08f7d08640d911f208443a
Marc SIBERT <Marcussacapuces91@users.noreply.github.com>
2026-08-13 21:25:42 +0200

    Update ollama usage to include client initialization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ollama
 
 if __name__ == "__main__":
-  resp = ollama.generate("gemma4:cloud", prompt="Pourquoi le ciel est bleu ?")
+  client = ollama.client("https://ollama.com")
+  resp = client.generate("gemma4:cloud", prompt="Pourquoi le ciel est bleu ?")
   print(resp)
