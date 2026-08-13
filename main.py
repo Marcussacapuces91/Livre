40ad336376225a3411f96cf835a5baf1a699865b
Marc SIBERT <Marcussacapuces91@users.noreply.github.com>
2026-08-13 22:16:26 +0200

    Change model and save response as HTML
    Updated model used for generation and added markdown conversion for response output.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from ollama import Client
 import os
+import markdown
 
 if __name__ == "__main__":
   client = Client(
     host='https://ollama.com',
     headers={'Authorization': 'Bearer ' + os.environ.get('OLLAMA_API_KEY')}
   )
-  resp = client.generate("gemma4:cloud", prompt="Pourquoi le ciel est bleu ?")
+  resp = client.generate("gpt-oss:120b", prompt="Pourquoi le ciel est bleu ?")
   print(resp)
+  with open("response.html", "w", encoding="utf-8", errors="xmlcharrefreplace") as output_file:
+    output_file.write(markdown.markdown(resp['response']))
+    
